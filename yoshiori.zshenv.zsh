72645a4dbfcf134438088ec29779d86f72d75c11
Lincoln Lee <git@lincoln.hk>
2015-02-23 09:42:08 +0800

    Update alias and PATH

--- yoshiori.zshenv.zsh
+++ yoshiori.zshenv.zsh
@@ -1,5 +1,5 @@
 #export
-export PATH=/usr/local/sbin:$HOME/.cask/bin:$HOME/bin:/usr/local/bin:$PATH:/usr/local/share/npm/bin
+export PATH=/opt/chefdk/bin:/usr/local/sbin:$HOME/.cask/bin:$HOME/bin:/usr/local/bin:$PATH:/usr/local/share/npm/bin:/usr/local/opt/go/libexec/bin
 export LANG=en_US.UTF-8
 export LC_CTYPE="en_US.UTF-8"
 
@@ -10,8 +10,11 @@ export PYTHONSTARTUP=$HOME/.pythonrc.py
 export ANDROID_HOME=/usr/local/opt/android-sdk
 
 #alias
-alias ls='els'
 alias git='/usr/local/bin/git'
+alias e='emacsclient -t'
+alias ec='emacsclient -c'
+alias vim='emacsclient -t'
+alias vi='emacsclient -t'
 
 # rvm
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
