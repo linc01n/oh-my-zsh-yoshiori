efb466f42f2c14b918dec9515aebb32b9aeac42e
Lincoln Lee <git@lincoln.hk>
2015-12-05 15:57:20 +0800

    Add coreutils

--- yoshiori.zshenv.zsh
+++ yoshiori.zshenv.zsh
@@ -1,5 +1,5 @@
 #export
-export PATH=/opt/chefdk/bin:/usr/local/sbin:$HOME/.cask/bin:$HOME/bin:/usr/local/bin:$PATH:/usr/local/share/npm/bin:/usr/local/opt/go/libexec/bin:./bin
+export PATH=/usr/local/opt/coreutils/libexec/gnubin:/opt/chefdk/bin:/usr/local/sbin:$HOME/.cask/bin:$HOME/bin:/usr/local/bin:$PATH:/usr/local/share/npm/bin:/usr/local/opt/go/libexec/bin:./bin
 export LANG=en_US.UTF-8
 export LC_CTYPE="en_US.UTF-8"
 
@@ -15,6 +15,7 @@ alias e='emacsclient -t'
 alias ec='emacsclient -c'
 alias vim='emacsclient -t'
 alias vi='emacsclient -t'
+alias ls='ls --color'
 
 # rvm
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
