c732e59880eab1002e5ec96de593074467cb23dd
Lincoln Lee <git@lincoln.hk>
2016-06-02 17:08:57 +0800

    Add go config

--- yoshiori.zshenv.zsh
+++ yoshiori.zshenv.zsh
@@ -1,5 +1,4 @@
 #export
-export PATH=/usr/local/opt/coreutils/libexec/gnubin:/opt/chefdk/bin:/usr/local/sbin:$HOME/.cask/bin:$HOME/bin:/usr/local/bin:$PATH:/usr/local/share/npm/bin:/usr/local/opt/go/libexec/bin:./bin
 export LANG=en_US.UTF-8
 export LC_CTYPE="en_US.UTF-8"
 
@@ -8,6 +7,9 @@ export EDITOR=emacs
 export JAVA_OPTS='-Dfile.encoding=UTF-8'
 export PYTHONSTARTUP=$HOME/.pythonrc.py
 export ANDROID_HOME=/usr/local/opt/android-sdk
+export GOPATH=~/projects/go
+
+export PATH=/usr/local/opt/coreutils/libexec/gnubin:/opt/chefdk/bin:/usr/local/sbin:$HOME/.cask/bin:$HOME/bin:/usr/local/bin:$PATH:/usr/local/share/npm/bin:/usr/local/opt/go/libexec/bin:./bin:$GOPATH/bin
 
 #alias
 alias git='/usr/local/bin/git'
